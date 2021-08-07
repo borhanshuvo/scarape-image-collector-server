fbfc91c8c3a8ba70ed95e32c1bf63689d9f550ef
borhanshuvo <borhan015@gmail.com>
2021-08-07 13:17:09 +0600

    login & registration add

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-  const sql = "select * from users";
+  const sql = "SELECT * FROM users";
   db.query(sql, (err, result) => {
     if (err) {
       res.send(err);
@@ -27,6 +27,17 @@ app.get("/users", (req, res) => {
   });
 });
 
+// app.get("/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   const sql = "SELECT * FROM users WHERE id=?";
+//   db.query(sql, [id], (err, result) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.send(result);
+//   });
+// });
+
 app.post("/regstration", (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
@@ -34,9 +45,21 @@ app.post("/regstration", (req, res) => {
   const sql = "INSERT INTO users (username, email, password) VALUES(?,?,?)";
   db.query(sql, [username, email, password], (err, result) => {
     if (err) {
-      return console.log(err);
+      res.send(err);
     }
-    return console.log(result);
+    res.send(result);
+  });
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const sql = "SELECT id, username, email FROM users WHERE email=? AND password=?";
+  db.query(sql, [email, password], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
   });
 });
 
